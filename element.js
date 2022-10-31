d6290347be1fa8ca98c0ee974259f1a048222277
monotypeee <spair0039@gmail.com>
2022-10-31 12:23:27 +0900

    ☕:: 카드 출력 js

--- element.js
+++ element.js
@@ -1,11 +1,25 @@
 const cardListBox = document.querySelector('#cardList');
+const boxes = document.querySelectorAll(`#cardMain`);
 
-function loadList(){
-    const newTT = document.createElement('span');
+function loadList(arrIndex){
+    const newTT = document.createElement('div');
     newTT.id = 'cardMain';
-    newTT.innerHTML = '';
-    cardListBox.appendChild(newTT); 
+    cardListBox.appendChild(newTT);
+    newTT.innerHTML = `            <div id="cardImg"></div>
+    <span>
+        <div id="cardNameK"></div>
+        <div id="cardNameLt"></div>
+        <div id="cardMeridianWhen"></div>
+        <div id="cardTags">
+            <span id="cardTag"></span>
+        </div>
+    </span>`;
+    newTT.querySelector('#cardNameK').innerText = starData[arrIndex].nameKo;
+    newTT.querySelector('#cardNameLt').innerText = starData[arrIndex].nameLt;
+    newTT.querySelector('#cardMeridianWhen').innerText = `${starData[arrIndex].whenMeridian} 최고`;  
+    newTT.querySelectorAll('#cardTag')[0].innerText = starData[arrIndex].tags[0]; 
 }
 
-
-loadList();
+for(let i = 0;i<=starData.length-1;i++){
+    loadList(i);
+}
