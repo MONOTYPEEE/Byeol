c28964452b70f9d7507167d8a4fe27e9e91214e8
monotypeee <spair0039@gmail.com>
2022-11-18 17:44:30 +0900

    🎨:: 태그 탭 이동 구현

---
+++ 신나는자바스크립트/headerBolder.js
@@ -0,0 +1,32 @@
+const nsx = document.querySelectorAll('.topBarSt');
+
+switch(urlParam){
+    case 'zodiac':{
+        nsx[6].classList.add('selected');
+        break;
+    }
+
+    case 'authmn':{
+        nsx[3].classList.add('selected');
+        break;
+    }
+
+    case 'summer':{
+        nsx[2].classList.add('selected');
+        break;
+    }
+    
+    case 'winter':{
+        nsx[4].classList.add('selected');
+        break;
+    }
+    
+    case 'spring':{
+        nsx[1].classList.add('selected');
+    }
+
+    case 'northen':{
+        nsx[5].classList.add('selected');
+        break;
+    }
+}
