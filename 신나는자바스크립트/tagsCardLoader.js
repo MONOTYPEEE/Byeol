71b98dabceff5c343cca2d59dfb582c300291e75
monotypeee <spair0039@gmail.com>
2022-11-18 09:23:47 +0900

    ☕:: 태그 페이지 기본 로드 구조

--- 신나는자바스크립트/tagsCardLoader.js
+++ 신나는자바스크립트/tagsCardLoader.js
@@ -1,2 +1,17 @@
 const urlParam = new URL(location).searchParams.get('tagname');
-console.log(urlParam);
+let a = 0;
+
+switch(urlParam){
+    case 'zodiac':{
+        starData.forEach(function(element,index,array){
+            if(element.emoji!=undefined){
+                loadList(element,index,array);
+            }
+        });
+    }
+
+    case 'authmn':
+    case 'summer':
+    case 'winter':
+    case 'spring':
+}
