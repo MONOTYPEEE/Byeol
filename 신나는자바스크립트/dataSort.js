838d19d207e3cf0a0e99ad3de457da4eb3d81d74
monotypeee <spair0039@gmail.com>
2022-11-21 08:25:18 +0900

    ☕:: 날짜 지난 별자리 후방 이동 코드

--- 신나는자바스크립트/dataSort.js
+++ 신나는자바스크립트/dataSort.js
@@ -1,3 +1,9 @@
 starData.sort((a,b)=>{
     return new Date(a.whenMeridian) - new Date(b.whenMeridian);
 });
+
+starData.forEach(function(element,index,array){
+    if(Date.parse(element.whenMeridian) - new Date()<=0){
+        starData.push(starData.shift());
+    }
+});
