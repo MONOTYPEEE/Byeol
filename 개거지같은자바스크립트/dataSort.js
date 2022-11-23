061830ce9d9eac59a95f2110755fc23125579644
monotypeee <spair0039@gmail.com>
2022-11-23 23:42:13 +0900

    ☕:: 날짜정렬 성공(즐겁다)

--- 개거지같은자바스크립트/dataSort.js
+++ 개거지같은자바스크립트/dataSort.js
@@ -2,10 +2,18 @@ starData.sort((a,b)=>{
     return dayjs(a.whenMeridian).diff(b.whenMeridian);
 });
 
+console.log(starData);
+
 starData.forEach(function(element,index,array){
     if(
         dayjs(dayjs().subtract(15,"day")).isAfter(dayjs(element.whenMeridian),'day')
     ){
-            starData.push(starData.shift());
+        console.log(`${element.nameKo} shifted`);
+        starData.push(starData.shift());
+    }
+    else{
+        console.log("nah")
     }
 });
+
+starData.unshift(starData.pop());
