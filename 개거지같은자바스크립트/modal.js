1b9c05ba5cea0665b5cf78eef85ffabf53647f49
monotypeee <spair0039@gmail.com>
2022-11-24 19:01:11 +0900

    ☕:: 모달 스위치 구현

--- 개거지같은자바스크립트/modal.js
+++ 개거지같은자바스크립트/modal.js
@@ -1,2 +1,18 @@
 const modalBgBg = document.querySelector("#infoModalBg");
+const modalBox = document.querySelector('#infoModal');
 let isModalOn = false;
+
+function modalVisableSwitch(){
+    if(isModalOn==true){
+        modalBgBg.classList.add('hiden');
+        modalBox.classList.add('hiden');
+        isModalOn=false;
+    }
+    else{
+        modalBgBg.classList.remove('hiden');
+        modalBox.classList.remove('hiden');
+        isModalOn=true;
+    }
+}
+
+modalBgBg.addEventListener('click',modalVisableSwitch);
