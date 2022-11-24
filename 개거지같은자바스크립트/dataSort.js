271f3271296c2723457e060d83965fa087a62931
monotypeee <spair0039@gmail.com>
2022-11-24 18:53:08 +0900

    ☕:: sort 상태 출력 테스트용 코드 주석화

--- 개거지같은자바스크립트/dataSort.js
+++ 개거지같은자바스크립트/dataSort.js
@@ -2,18 +2,16 @@ starData.sort((a,b)=>{
     return dayjs(a.whenMeridian).diff(b.whenMeridian);
 });
 
-console.log(starData);
-
 starData.forEach(function(element,index,array){
     if(
         dayjs(dayjs().subtract(15,"day")).isAfter(dayjs(element.whenMeridian),'day')
     ){
-        console.log(`${element.nameKo} shifted`);
+        //console.log(`${element.nameKo} shifted`);
         starData.push(starData.shift());
     }
-    else{
-        console.log("nah")
-    }
+    // else{
+    //     console.log("nah")
+    // }
 });
 
 starData.unshift(starData.pop());
