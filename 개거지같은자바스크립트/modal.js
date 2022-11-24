a8dfd9212db84cf25544a6547ff5e8b243f302f9
monotypeee <spair0039@gmail.com>
2022-11-24 19:23:19 +0900

    ☕:: 모달 스크롤 방지 옵션 추가

--- 개거지같은자바스크립트/modal.js
+++ 개거지같은자바스크립트/modal.js
@@ -1,16 +1,19 @@
 const modalBgBg = document.querySelector("#infoModalBg");
 const modalBox = document.querySelector('#infoModal');
+const htmlBOdy = document.querySelector('body');
 let isModalOn = false;
 
 function modalVisableSwitch(){
     if(isModalOn==true){
         modalBgBg.classList.add('hiden');
         modalBox.classList.add('hiden');
+        htmlBOdy.style.overflow = "hidden";
         isModalOn=false;
     }
     else{
         modalBgBg.classList.remove('hiden');
         modalBox.classList.remove('hiden');
+        htmlBOdy.style.overflow = "auto";
         isModalOn=true;
     }
 }
