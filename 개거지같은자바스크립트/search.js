68fa3b470f846560179ef52a354835fb2a2d533e
monotypeee <spair0039@gmail.com>
2022-11-29 20:02:31 +0900

    ☕:: 리제로부터시작하는검색기능구현하기

--- 개거지같은자바스크립트/search.js
+++ 개거지같은자바스크립트/search.js
@@ -1,7 +1,21 @@
 const searchBar = document.querySelector("#searchInput");
 
 function searchToggler(){
-    
+    if(searchBar==''){
+
+    }
+};
+
+function allCardHiden(){
+    cardListBox.querySelectorAll("#cardMain").forEach(function(elm){
+        elm.classList.add('hiden');
+    });
+};
+
+function allCardVisable(){
+    cardListBox.querySelectorAll("#cardMain").forEach(function(elm){
+        elm.classList.remove('hiden');
+    });
 };
 
 searchBar.addEventListener('input',searchToggler);
