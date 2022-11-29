6a7f07a2cbf6c3c6add886136b96801501508a40
monotypeee <spair0039@gmail.com>
2022-11-29 19:31:39 +0900

    ☕:: 검색하면 사라지긴 합니다

--- 개거지같은자바스크립트/search.js
+++ 개거지같은자바스크립트/search.js
@@ -1,17 +1,13 @@
 const searchBar = document.querySelector("#searchInput");
 
 searchBar.addEventListener('input',function(){
-    if(searchBar!=''){
-
-    }
-
     cardListBox.querySelectorAll("#cardMain").forEach(function(elemnt){
         if(elemnt.querySelector('#cardNameK').innerHTML.includes(searchBar.value)||
         elemnt.querySelector('#cardNameLt').innerHTML.toLowerCase().includes(searchBar.value)||
         elemnt.querySelector('#cardNameLt').innerHTML.toUpperCase().includes(searchBar.value)||
         elemnt.querySelector('#cardNameLt').innerHTML.includes(searchBar.value)){
             //한글이름, 소문자화라틴어, 대문자화라틴어, 라틴어 검사 
-            console.log(elemnt);
+            elemnt.classList.add('notSearchRes');
         }
     });
     console.log("end of search");
