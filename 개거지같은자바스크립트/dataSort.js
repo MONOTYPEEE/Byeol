aeabf8319cd860db0d9c63812cf38199eb37fa45
monotypeee <spair0039@gmail.com>
2022-12-14 15:21:08 +0900

    🌫️:: 각주 삭제

--- 개거지같은자바스크립트/dataSort.js
+++ 개거지같은자바스크립트/dataSort.js
@@ -6,12 +6,7 @@ starData.forEach(function(element,index,array){
     if(
         dayjs(dayjs().subtract(15,"day")).isAfter(dayjs(element.whenMeridian),'day')
     ){
-        //console.log(`${element.nameKo} shifted`);
         starData.push(starData.shift());
     }
-    // else{
-    //     console.log("nah")
-    // }
 });
-
 starData.unshift(starData.pop());
